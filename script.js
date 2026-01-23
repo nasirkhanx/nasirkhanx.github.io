0ce8c96fd6f6e8bc6243650affa33b88e7181b50
nasirkhanx <146998180+nasirkhanx@users.noreply.github.com>
2026-01-23 23:51:11 +0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelectorAll('a').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
 });
